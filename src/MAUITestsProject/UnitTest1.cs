64994160232a35542fedc95cdc9f76c3fb3381b8
L00141197 <L00141197@atu.ie>
2023-12-11 12:30:38 +0000

    source code

---
+++ src/MAUITestsProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+namespace MAUITestsProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void DummyTest1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void DummyTest2()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void DummyTest3()
+        {
+            Assert.True(true);
+        }
+    }
+}
